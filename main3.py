d408ca0df67ad4d57457d38dbb3b33c7373647d9
Raghad <rghdhnya@gmail.com>
2023-05-30 18:52:33 +0300

    concatenate_dictionaries

--- main3.py
+++ main3.py
@@ -15,3 +15,17 @@ School = ["Ali", "Basma"]
 
 friends.extend(employees + School)
 print(friends)
+
+def concatenate_dictionaries(*dicts):
+    dict = {}
+    for i in dicts:
+        dict.update(i)
+    return dict
+
+
+dict1 = {'1': 10, '2': 20}
+dict2 = {'3': 30, '4': 40}
+dict3 = {'5': 50, '6': 60}
+
+new_dict = concatenate_dictionaries(dict1, dict2, dict3)
+print(new_dict)
